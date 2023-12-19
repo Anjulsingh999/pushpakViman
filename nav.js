c1c4eec94b7a5ded63d7018a2ca18e764a0c1139
Anjul Singh <118563318+Anjulsingh999@users.noreply.github.com>
2023-12-19 12:10:04 +0530

    update dropdown and  add training & service page

---
+++ nav.js
@@ -0,0 +1,12 @@
+
+
+var one = document.getElementById("cross");
+  let ok = document.getElementById('phone');
+  var navi = document.getElementById('navy');
+one.onclick=()=>{
+  ok.style.display="none";
+}
+navi.onclick=()=>{
+  ok.style.display="flex";
+  // alert('ji');
+}
