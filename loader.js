7ea7263092274d48cc5bc3f28ebcff2b82626e8e
Anjul Singh <118563318+Anjulsingh999@users.noreply.github.com>
2023-12-23 13:44:08 +0530

    add loader page

---
+++ loader.js
@@ -0,0 +1,10 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Simulate content loading
+    setTimeout(function () {
+        // Hide the loader
+        document.getElementById("loader-wrapper").style.display = "none";
+        // Show the content
+        document.querySelector(".content").style.display = "block";
+    }, 4000); // Adjust the timeout duration based on your actual content loading time
+});
